1a0882f992afe490805be554f2a46981aa83cf52
Ivan Buncic <ivan@triplsi.com>
2024-10-07 11:26:27 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,8 @@
-import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
-import { feedPlugin } from "@11ty/eleventy-plugin-rss";
-import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
-import pluginNavigation from "@11ty/eleventy-navigation";
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "./node_modules/@11ty/eleventy";
+import { feedPlugin } from "./node_modules/@11ty/eleventy-plugin-rss";
+import pluginSyntaxHighlight from "./node_modules/@11ty/eleventy-plugin-syntaxhighlight";
+import pluginNavigation from "./node_modules/@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "./node_modules/@11ty/eleventy-img";
 
 import pluginFilters from "./_config/filters.js";
 
